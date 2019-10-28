32926af648a54324bcc7f8662657ad50101c1a45
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-10-28 10:34:46 +0300

    Create Task3_2.py

---
+++ ДЗ 3/Task3_2.py
@@ -0,0 +1,13 @@
+a_set = {}
+a_set['first'] = 1
+a_set['second'] = 2
+a_set['third'] = 3
+a_set[4] = 'fourth'
+
+del a_set['third']
+del a_set[4]
+
+print(a_set['first'])
+print(a_set['second'])
+
+a_set['new_one'] = a_set['first'] + a_set['second

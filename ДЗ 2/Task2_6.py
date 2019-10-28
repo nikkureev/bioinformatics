51b4dddc9e6e7dd50bae18c15d60ee573e1fb140
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-10-28 09:41:32 +0300

    Update Task2_6.py

--- ДЗ 2/Task2_6.py
+++ ДЗ 2/Task2_6.py
@@ -2,3 +2,12 @@
 
 elements = [1, 2, ('fruit', 5)]
 elements.reverse()
+
+list(reversed(elements))
+# в последнем случае исходный список меняться не будет
+
+elements_reversed = []
+for i in a[::-1]:
+    elements_reversed.append(i)
+
+    

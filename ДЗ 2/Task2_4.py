c88024e6b6f007275d5d53de0b25fdff8106f841
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-10-28 10:06:02 +0300

    Update Task2_4.py

--- ДЗ 2/Task2_4.py
+++ ДЗ 2/Task2_4.py
@@ -2,7 +2,16 @@
 
 list_1 = [1, 2, 3, 4]
 list_2 = [5, 6, 7, 8]
+
+# создаём новый список
 list_3 = []
 for i in range(len(list_1)):
     list_3.append(list_1[i] + list_2[i])
+    # выводим print'ом
+    print(list_1[i] + list_2[i], end=' ')
+# выводим новый список 
 print(list_3)
+
+
+
+

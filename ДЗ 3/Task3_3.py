cfc4c84912cc73b2680e383b953e1445a3f65aa9
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-10-30 15:31:04 +0300

    Update Task3_3.py

--- ДЗ 3/Task3_3.py
+++ ДЗ 3/Task3_3.py
@@ -1,1 +1,14 @@
-a_set = {}
+a_set = {'first': 1, 'second': 2, 'third': 3}
+for key, val in a_set.items():
+    print(key, val)
+
+###
+
+a_set = {'first': 1, 'second': 2, 'third': 3}
+for key in a_set:
+    print(key, a_set[key]
+
+
+
+
+

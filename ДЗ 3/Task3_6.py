0e61f05bc281c1d533d6fe94c844c5ce0b4f124d
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-11-02 23:21:59 +0300

    Update Task3_6.py

--- ДЗ 3/Task3_6.py
+++ ДЗ 3/Task3_6.py
@@ -1,15 +1,22 @@
 a_list = ['one', 'one', 'two', 'three']
 a_set = set(a_list)
 a_tuple = tuple(a_list)
-print(a_list)
-print(a_set)
-print(a_tuple)
 a_str = ''
 for i in a_list:
 	a_str += i
-print(a_str)
-a_set_2 = set(a_str)
-print(a_set_2)
-a_tuple 
-''.join(tup)
+
+a_tuple_2 = ('one', 'one', 'two', 'three')
+a_str_2 = ''.join(a_tuple_2)
+a_list_2 = list(a_tuple_2)
+a_set_2 = set(a_tuple_2)
+
+a_set_3 = {'one', 'two', 'three'}
+a_str_3 = ''.join(a_set_3)
+a_list_3 = list(a_set_3)
+a_tuple_3 = tuple(a_set_3)
+
+a_str_4 = 'one_one_two_three'
+a_list_4 = [i for i in a_str_4.split('_')]
+a_tuple_4 = (i for i in a_str_4.split('_'))
+a_set_4 = {i for i in a_str_4.split('_')}
 

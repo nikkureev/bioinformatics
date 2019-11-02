a4543fc79bc8b255711df4d3486f3f0c6835b4b3
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-11-02 22:58:44 +0300

    Create Task3_6.py

---
+++ ДЗ 3/Task3_6.py
@@ -0,0 +1,15 @@
+a_list = ['one', 'one', 'two', 'three']
+a_set = set(a_list)
+a_tuple = tuple(a_list)
+print(a_list)
+print(a_set)
+print(a_tuple)
+a_str = ''
+for i in a_list:
+	a_str += i
+print(a_str)
+a_set_2 = set(a_str)
+print(a_set_2)
+a_tuple 
+''.join(tup)
+

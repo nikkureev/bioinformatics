ff55a8e6e8933f094d4658bae7b2b66a4bfbcf96
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-11-03 14:11:23 +0300

    Create Task4_6.py

---
+++ ДЗ 4/Task4_6.py
@@ -0,0 +1,19 @@
+def moda(l):
+    all_dict = {}
+    for i in l:
+        if i not in all_dict:
+            all_dict[i] = 1
+        else:
+            all_dict[i] += 1
+
+    max_moda = 0
+    for i in all_dict.keys():
+        if all_dict[i] > max_moda:
+            max_moda = all_dict[i]
+
+    new_list = []
+    for i in all_dict.keys():
+        if all_dict[i] == max_moda:
+            new_list.append(i)
+
+    return new_list

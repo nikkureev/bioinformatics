1bf9f064a0598b24e9b34d51efd1ae10e55b4be7
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-11-17 09:27:31 +0300

    Create Task5_1.py

---
+++ ДЗ 5/Task5_1.py
@@ -0,0 +1,8 @@
+print('%a has %b dollars' % (name, amount_of_money))
+a = '%s has %s dollars' % ('Anna', 75)
+
+print('{} has {} dollars'.format(name, amount_of_money))
+a = '{} has {} dollars'.format('Anna', 75)
+
+print(f'{name} has {amount_of_money} dollars')
+a = f'{"Anna"} has {75} dollars'

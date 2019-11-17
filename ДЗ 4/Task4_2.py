240aaf7fe9ee7c4e1c9b34063fe2a7859eacef91
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-11-17 11:09:57 +0300

    Update Task4_2.py

--- ДЗ 4/Task4_2.py
+++ ДЗ 4/Task4_2.py
@@ -1,8 +1,11 @@
 def fiboncci(number):
     counter = 0
     count = [0, 1]
-    while counter < number - 1:
-        n = count[-1] + count[-2]
-        count.append(n)
-        counter += 1
-    print(count[-1])
+    if number == 0:
+        print(0)
+    else:
+        while counter < number - 1:
+            n = count[-1] + count[-2]
+            count.append(n)
+            counter += 1
+        print(count[-1])

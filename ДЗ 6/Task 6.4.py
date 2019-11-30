54585a23af85facf10a3082ab7bad905aff8ef89
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-11-30 14:40:47 +0300

    Update Task 6.4.py

--- ДЗ 6/Task 6.4.py
+++ ДЗ 6/Task 6.4.py
@@ -3,14 +3,36 @@ graph = {'1': ['2', '3'],
          '3': ['1', '6'],
          '4': ['2'],
          '5': ['2', '6'],
-         '6': ['3', '5']}
+         '6': ['3', '5'],
+         '7': ['8'],
+         '8': ['7'],
+         '9': ['10', '7'],
+         '10': ['9']}
 
 
-def dfs(graph, start):
-    visited, stack = [], [start]
-    while stack:
-        vertex = stack.pop()
-        if vertex not in visited:
-            visited.append(vertex)
-            stack.extend(set(graph[vertex]) - set(visited))
-    return visited
+def comp_count(graph):
+
+    def dfs(graph, start):
+        visited, stack = [], [start]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.append(vertex)
+                stack.extend(set(graph[vertex]) - set(visited))
+        return visited
+
+    vis_list = []
+    for i in range(1, len(graph)):
+        vis_list.append(dfs(graph, str(i)))
+
+    comp = 1
+    for i in range(len(vis_list) - 1):
+        for j in vis_list[i]:
+            if j not in vis_list[i + 1]:
+                comp += 1
+                break
+
+    print(comp)
+
+
+comp_count(graph)

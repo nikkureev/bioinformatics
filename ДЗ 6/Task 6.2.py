2b18b0b2d1366b9f80adab2713085abb3f8d9152
nikkureev <32942796+nikkureev@users.noreply.github.com>
2019-12-01 09:40:03 +0300

    Update Task 6.2.py

--- ДЗ 6/Task 6.2.py
+++ ДЗ 6/Task 6.2.py
@@ -1,7 +1,7 @@
 def cop(in_file, out_file, start, end):
     with open(in_file, 'a') as o:
         with open(out_file, 'r') as i:
-            for j in range(end):
+            for j in range(end - 1):
                 r = i.readline()
-                if j >= start:
+                if j >= start - 1:
                     o.writelines(r)

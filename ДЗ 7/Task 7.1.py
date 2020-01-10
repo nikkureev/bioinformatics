183805a91c8b4aecc347fcd13778e7711da98b87
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-01-10 13:49:49 +0300

    Update Task 7.1.py

--- ДЗ 7/Task 7.1.py
+++ ДЗ 7/Task 7.1.py
@@ -1,8 +1,10 @@
 from Bio import SeqIO
 
-path_in, path_out, min_len = str(input())
-seqs = []
-for record in SeqIO.parse(path_in, "fastq"):
-    if len(record.seq) > min_len:
-        seqs.append(record)
-SeqIO.write(seqs, path_out, 'fasta')
+
+def select():
+    path_in, path_out, min_len = str(input())
+    seq = []
+    for record in SeqIO.parse(path_in, "fastq"):
+        if len(record.seq) > min_len:
+            seq.append(record)
+    SeqIO.write(seq, path_out, 'fasta')

616e3b2ebe4db90520d81fc500d5b89a0ca71313
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-01-10 14:44:03 +0300

    Update Task 7.2.py

--- ДЗ 7/Task 7.2.py
+++ ДЗ 7/Task 7.2.py
@@ -1,7 +1,8 @@
 from Bio import SeqIO
+import matplotlib.pyplot as plt
 
 
-def align(path_in, match_score, miss_match_score, gap_score):
+def align(path_in, match_score, miss_match_score, gap_score, glob_align=True):
     sequences = []
     for record in SeqIO.parse(path_in, "fasta"):
         sequences.append(record.seq)
@@ -9,10 +10,13 @@ def align(path_in, match_score, miss_match_score, gap_score):
 
     matrix[0] = [i for i in range(len(matrix[0]))]
 
-    n = 0
+    if glob_align:
+        n, m = gap_score, gap_score
+    else:
+        n, m = 0, 0
     for i in matrix:
         i[0] = n
-        n += 1
+        n += m
 
     d = gap_score
     for i in range(1, len(matrix)):
@@ -21,29 +25,54 @@ def align(path_in, match_score, miss_match_score, gap_score):
                 g = match_score
             else:
                 g = miss_match_score
-            matrix[i][j] = min(matrix[i - 1][j - 1] + g, matrix[i - 1][j] + d, matrix[i][j - 1] + d)
+            if glob_align:
+                matrix[i][j] = min(matrix[i - 1][j - 1] + g, matrix[i - 1][j] + d, matrix[i][j - 1] + d)
+            else:
+                if min(matrix[i - 1][j - 1] + g, matrix[i - 1][j] + d, matrix[i][j - 1] + d) > 0:
+                    matrix[i][j] = 0
+                else:
+                    matrix[i][j] = -1
 
+    for i in matrix:
+        print(i)
     score = 0
     seq = []
     i = len(matrix) - 1
     j = len(matrix[0]) - 1
 
-    while i != 0 or j != 0:
+    coordinates = [[i, j]]
+    while i > 0 or j > 0:
         if min(matrix[i - 1][j - 1], matrix[i][j - 1], matrix[i - 1][j]) == matrix[i - 1][j - 1]:
             seq.append([str(sequences[1][i - 1]), str(sequences[0][j - 1])])
             i -= 1
             j -= 1
             score += matrix[i - 1][j - 1]
+            coordinates.append([i - 1, j - 1])
         elif min(matrix[i - 1][j - 1], matrix[i][j - 1], matrix[i - 1][j]) == matrix[i][j - 1]:
             seq.append(['-', str(sequences[0][j - 1])])
             i = i
             j -= 1
             score += matrix[i][j - 1]
+            coordinates.append([i - 1, j])
         elif min(matrix[i - 1][j - 1], matrix[i][j - 1], matrix[i - 1][j]) == matrix[i - 1][j]:
             seq.append([str(sequences[1][i - 1]), '-'])
             i -= 1
             j = j
             score += matrix[i - 1][j]
+            coordinates.append([i, j - 1])
+
+    coordinates.sort()
+    #coordinates.remove([-1, -1])
+    x, y = [], []
+    for i in coordinates:
+        x.append(i[0])
+        y.append(i[1])
+    fig = plt.subplot()
+    fig.plot(x, y)
+    fig.minorticks_on()
+    fig.grid(which='major')
+    fig.grid(which='minor')
+    plt.show()
 
     s1 = ''
     seq.reverse()
@@ -54,5 +83,9 @@ def align(path_in, match_score, miss_match_score, gap_score):
     for i in seq:
         s2 += str(i[1])
 
-    align_result = (score, s1, s2)
+    align_result = (score, s1.upper(), s2.upper())
     return align_result
+
+
+result = align('C:/Theileria/seq2.txt', -2, 3, 2, glob_align=True)
+print(result[0], result[1], result[2], sep='\n')

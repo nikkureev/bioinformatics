03fc7994124df25c00fc8d6ddcc9636318a28dd4
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-01-16 14:52:08 +0300

    Update Task 7.1.py

--- ДЗ 7/Task 7.1.py
+++ ДЗ 7/Task 7.1.py
@@ -1,8 +1,7 @@
 from Bio import SeqIO
 
 
-def select():
-    path_in, path_out, min_len = str(input())
+def select(path_in, path_out, min_len=50):
     seq = []
     for record in SeqIO.parse(path_in, "fastq"):
         if len(record.seq) > min_len:

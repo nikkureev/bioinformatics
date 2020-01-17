4586125f932e624583f42da73b47ea9659c6b9fc
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-01-17 21:55:35 +0300

    Update Task 11.5.py

--- ДЗ 11/Task 11.5.py
+++ ДЗ 11/Task 11.5.py
@@ -7,10 +7,10 @@ def serpinsky_triangle(line_len):
     b = [line_len / 2, line_len]
     c = [line_len, 0]
     ran_point = [random.randint(0, line_len), random.randint(0, line_len)]
-    f = plt.scatter(a[0], a[1])
-    f = plt.scatter(b[0], b[1])
-    f = plt.scatter(c[0], c[1])
-    f = plt.scatter(ran_point[0], ran_point[1])
+    plt.scatter(a[0], a[1])
+    plt.scatter(b[0], b[1])
+    plt.scatter(c[0], c[1])
+    plt.scatter(ran_point[0], ran_point[1])
     for i in range(2000):
         n = random.randint(1, 3)
         if n == 1:
@@ -19,7 +19,7 @@ def serpinsky_triangle(line_len):
             ran_point = [abs(b[0] + ran_point[0]) / 2, abs(b[1] + ran_point[1]) / 2]
         elif n == 3:
             ran_point = [abs(c[0] + ran_point[0]) / 2, abs(c[1] + ran_point[1]) / 2]
-        f = plt.scatter(ran_point[0], ran_point[1])
+        plt.scatter(ran_point[0], ran_point[1])
     plt.show()
 
 

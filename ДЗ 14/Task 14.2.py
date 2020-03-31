830dd80a81051d2398af1174bb4f0c91aa4089b3
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-03-31 08:44:58 +0300

    Update Task 14.2.py

--- ДЗ 14/Task 14.2.py
+++ ДЗ 14/Task 14.2.py
@@ -4,4 +4,4 @@ import pandas as pd
 data = pd.read_csv('C:/Theileria_seq/train.csv', delimiter=',')
 df = data[data['matches'] > data['matches'].mean()]
 df = df[['reads_all', 'mismatches', 'deletions', 'insertions']]
-print(df)
+df.to_csv('C:/Theileria_seq/train_part.csv')

f7fd2af7eb20897bb02d736e308464567922ed67
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-04-08 11:48:37 +0300

    Update Task 15.1.py

--- ДЗ 15/Task 15.1.py
+++ ДЗ 15/Task 15.1.py
@@ -3,9 +3,12 @@ import re
 
 file = 'C:/Theileria_seq/references.txt'
 out = 'C:/Theileria_seq/ftps.txt'
-with open(file, 'r') as f:
-    with open(out, 'w') as d:
-        for lines in f:
-            for i in re.findall('(ftp.(\w+.){9,12}[gz;\t, md5])', lines):
-                d.write(str(i[0]) + '\t')
-            d.write('\n')
+
+# This function obtain all ftps from your file
+def refiler(inp, out):
+    with open(inp, 'r') as f:
+        with open(out, 'w') as d:
+            for lines in f:
+                for i in re.findall('(ftp.(\w+.){9,12}[gz;\t, md5])', lines):
+                    d.write(str(i[0]) + '\t')
+                d.write('\n')

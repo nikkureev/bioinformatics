60f773965e0bf7d60ba4e928ea2220455fe1ecf4
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-04-08 12:28:29 +0300

    Update Task 15.2.py

--- ДЗ 15/Task 15.2.py
+++ ДЗ 15/Task 15.2.py
@@ -4,10 +4,10 @@ import re
 file = 'C:/Python/2430AD.txt'
 
 # This function obtain all numbers from your text file
-def refiler(inp):
+def number_finder(inp):
     with open(inp, 'r') as f:
         for lines in f:
             for i in re.findall('[0-9]{1,}', lines):
                 print(i)
 
-refiler(file)
+number_finder(file)

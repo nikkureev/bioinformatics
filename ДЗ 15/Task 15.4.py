766a9a82fbaf2c31b2ae2e89f4f2ef65b255c8aa
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-04-13 19:55:23 +0300

    Update Task 15.4.py

--- ДЗ 15/Task 15.4.py
+++ ДЗ 15/Task 15.4.py
@@ -6,7 +6,7 @@ file = 'C:/Python/2430AD.txt'
 def high_finder(inp):
     with open(inp, 'r') as f:
         for lines in f:
-            for i in re.findall('[A-Z]{1}[a-z\s]+!', lines):
+            for i in re.findall('/[A-Za-z,]*[a-z\s]+!/gi', lines):
                 print(i)
 
 high_finder(file)

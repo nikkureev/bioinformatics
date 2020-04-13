bb2b7ecb5b4fca77dcf68e5505dd95ea27e8ebf6
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-04-13 19:52:57 +0300

    Update Task 15.3.py

--- ДЗ 15/Task 15.3.py
+++ ДЗ 15/Task 15.3.py
@@ -7,7 +7,7 @@ file = 'C:/Python/2430AD.txt'
 def a_finder(inp):
     with open(inp, 'r') as f:
         for lines in f:
-            for i in re.findall('\w+a\w+', lines):
+            for i in re.findall('/\b[\w+]*a[\w+]*\b/gi', lines):
                 print(i)
 
 a_finder(file)

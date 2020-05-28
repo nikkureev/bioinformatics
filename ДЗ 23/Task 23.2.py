3b6f424994e5a6cdca25cc8780f3c118c2e6e8f7
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-05-29 01:41:07 +0300

    Create Task 23.2.py

---
+++ ДЗ 23/Task 23.2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+plt.axis('off')
+path = '/home/nikolay/cute.png'
+img = plt.imread(path, 0)
+plt.imshow(img)
+
+kernel = [[-1, 2, -1],
+          [2, -1, 2],
+          [-1, 2, -1]]
+
+modified = np.zeros_like(img)
+
+for row in range(1, img.shape[0] - 2):
+    for col in range(1, img.shape[1] - 2):
+        modified[row, col] = (img[row: row + 3, col: col + 3] * kernel).sum()
+
+plt.imshow(modified, cmap='gray')
+plt.show()
